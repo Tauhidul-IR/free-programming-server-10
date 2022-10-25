482b7303c7490cf24b3f9e65115295da9e66a04d
Tauhidul Islam Robin <robin13690@gmail.com>
2022-10-25 18:40:11 +0600

    single course

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const courses = require('./data/courses.json');
+
+
+app.get('/', (req, res) => {
+    res.send("API is Running")
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const course = courses.find(cr => cr.id === id);
+    res.send(course)
+})
+
+
+
+app.listen(port, () => {
+    console.log("runnig on port", port)
+})
